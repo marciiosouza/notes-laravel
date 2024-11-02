5ca16c1da79923a1b7e791e6620b559fafec1dfa
Marcio Souza <marciiosouza@outlook.com>
2024-11-02 17:25:56 -0300

    data validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,9 +13,20 @@ class AuthController extends Controller
     
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // Form validation
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+        
+        // GET user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
+        
     }
     
     public function logout()
