49b7db846ca8b8faa2bd03ba07f3631e9725bf1a
Marcio Souza <marciiosouza@outlook.com>
2024-11-02 10:33:11 -0300

    Create Routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "hello world";
+});
+
+Route::get('/about', function () {
+    echo "About Us";
 });
