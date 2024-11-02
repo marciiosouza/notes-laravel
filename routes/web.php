f02e693c1a2d30f31abd722cb04907204f9e1823
Marcio Souza <marciiosouza@outlook.com>
2024-11-02 12:38:53 -0300

    cleaning up project

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,3 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    echo "hello world";
-});
-
-Route::get('/about', function () {
-    echo "About Us";
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
